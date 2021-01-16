b1c782555fa625ed4882da970e27e5ae999ddbbb
Shreyas <shreyas.mm@somaiya.edu>
2021-01-16 14:41:31 +0530

    Backend Connected

--- angular-TM/src/app/services/movie.service.ts
+++ angular-TM/src/app/services/movie.service.ts
@@ -21,34 +21,34 @@ export class MovieService {
     private processHTTPMsgService: ProcessHTTPMsgService) { }
 
 
-    getList(category): Observable<Movie[]> {
-      return this.http.get<Movie[]>("https://themovieplace-backend.herokuapp.com/getMovieList", {params: {mov:category}}) 
-      .pipe(delay(2000))    
-      .pipe(catchError(this.processHTTPMsgService.handleError));
-      ;
-    }
+    // getList(category): Observable<Movie[]> {
+    //   return this.http.get<Movie[]>("https://themovieplace-backend.herokuapp.com/getMovieList", {params: {mov:category}}) 
+    //   .pipe(delay(2000))    
+    //   .pipe(catchError(this.processHTTPMsgService.handleError));
+    //   ;
+    // }
 
-  //  getList(category): Observable<Movie[]> {
-  //   return this.http.get<Movie[]>(apiURL + '3/movie/' + category + apiKey) 
-  //   .pipe(delay(2000))    
-  //   .pipe(catchError(this.processHTTPMsgService.handleError));
-  //   ;
-  // }
+   getList(category): Observable<Movie[]> {
+    return this.http.get<Movie[]>(apiURL + '3/movie/' + category + apiKey) 
+    .pipe(delay(2000))    
+    .pipe(catchError(this.processHTTPMsgService.handleError));
+    ;
+  }
 
   getImageBaseUrl() {
     return imageBaseurl;
    }
 
-  //  getCredits(movie): Observable<any>{
-  //    return this.http.get<any>(apiURL + '3/movie/'+movie+"/credits"+apiKey)
-  //    .pipe(catchError(this.processHTTPMsgService.handleError));
-  //  }
-
    getCredits(movie): Observable<any>{
-    return this.http.get<any>("https://themovieplace-backend.herokuapp.com/getCredits", {params: {mov:movie}})
-    .pipe(catchError(this.processHTTPMsgService.handleError));
+     return this.http.get<any>(apiURL + '3/movie/'+movie+"/credits"+apiKey)
+     .pipe(catchError(this.processHTTPMsgService.handleError));
+   }
 
-  }
+  //  getCredits(movie): Observable<any>{
+  //   return this.http.get<any>("https://themovieplace-backend.herokuapp.com/getCredits", {params: {mov:movie}})
+  //   .pipe(catchError(this.processHTTPMsgService.handleError));
+
+  // }
 
   search(query) {
     query = this.convertToSlug(query);
@@ -65,42 +65,42 @@ export class MovieService {
     return string;
   }
 
-  getMovie(id){
-    return this.http.get( "https://themovieplace-backend.herokuapp.com/getMovie", {params: {mov:id}})
-    .pipe(delay(3000))     
-    .pipe(catchError(this.processHTTPMsgService.handleError));
-
-  }
-
   // getMovie(id){
-  //   return this.http.get( apiURL + '3/movie/' +id+ apiKey+ "&language=en-US")
+  //   return this.http.get( "https://themovieplace-backend.herokuapp.com/getMovie", {params: {mov:id}})
   //   .pipe(delay(3000))     
   //   .pipe(catchError(this.processHTTPMsgService.handleError));
 
   // }
 
-  getCertification(id){
-    return this.http.get( "https://themovieplace-backend.herokuapp.com/getCertificate", {params: {mov:id}})
+  getMovie(id){
+    return this.http.get( apiURL + '3/movie/' +id+ apiKey+ "&language=en-US")
+    .pipe(delay(3000))     
     .pipe(catchError(this.processHTTPMsgService.handleError));
+
   }
 
   // getCertification(id){
-  //   return this.http.get( apiURL + '3/movie/' +id+ "/release_dates" + apiKey)
+  //   return this.http.get( "https://themovieplace-backend.herokuapp.com/getCertificate", {params: {mov:id}})
   //   .pipe(catchError(this.processHTTPMsgService.handleError));
-
   // }
 
-  getTrailer(id){
-    return this.http.get( "https://themovieplace-backend.herokuapp.com/getTrailer", {params: {mov:id}})
+  getCertification(id){
+    return this.http.get( apiURL + '3/movie/' +id+ "/release_dates" + apiKey)
     .pipe(catchError(this.processHTTPMsgService.handleError));
 
   }
 
   // getTrailer(id){
-  //   return this.http.get( apiURL + '3/movie/' +id+ "/videos" + apiKey)
+  //   return this.http.get( "https://themovieplace-backend.herokuapp.com/getTrailer", {params: {mov:id}})
   //   .pipe(catchError(this.processHTTPMsgService.handleError));
 
   // }
+
+  getTrailer(id){
+    return this.http.get( apiURL + '3/movie/' +id+ "/videos" + apiKey)
+    .pipe(catchError(this.processHTTPMsgService.handleError));
+
+  }
 
 }
 
